0a888a06aded1e92e48193d2430b0184a1b50b84
saikiran <bsaikiran618@gmail.com>
2020-02-21 22:06:12 +0530

    added fullpage.js

---
+++ index.js
@@ -0,0 +1,11 @@
+new fullpage('#fullpage', {
+	//options here
+	autoScrolling:true,
+	scrollHorizontally: true
+});
+
+//methods
+fullpage_api.setAllowScrolling(true);
+new fullpage('#fullpage', {
+	anchors:['firstSection', 'secondSection', 'thirdSection','fourthSection']
+});
